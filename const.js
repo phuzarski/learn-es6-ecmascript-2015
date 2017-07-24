b59a8f34c8349a72694d031a99ee9632b8c5dc6d
Patryk  Huzarski <phuzarski@gmail.com>
2017-07-24 13:20:58 +0200

    Const and reassigned

---
+++ const.js
@@ -0,0 +1,14 @@
+ const VALUE = {}
+ VALUE.foo = 'bar';
+ console.log('value: ', VALUE)
+ // I can add new property for the object for example VALUE.foo, VALUE.bar = 'xx', but can't change the initial value !!!!
+
+ if (true) {
+     const foo = 'bar';
+ console.log('foo int: ' + foo)
+
+    }
+ console.log('foo ext: ' + foo)
+
+
+ // not defined, because the declaration of console.log is outside the blog of the CONST declaration!
